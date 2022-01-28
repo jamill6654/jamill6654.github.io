da6856893fa3ac7e428450e688d52eb873c14484
jamill6654 <jamill6654@gmail.com>
2022-01-28 09:26:10 -0500

    third commit

--- js/app.js
+++ js/app.js
@@ -7,15 +7,29 @@ const aboutLink = document.querySelector(".about-link");
 const projectsLink = document.querySelector(".projects-link");
 const contactLink = document.querySelector(".contact-link");
 
-console.log(aboutLink);
 aboutLink.addEventListener('click', ()=> {
-    about.classList.add("shadow");
+    about.classList.add("shadows-text");
+    setTimeout(aboutShadow, 3500);
 });
+
+function aboutShadow(){
+    about.classList.remove("shadows-text");
+}
 
 projectsLink.addEventListener('click', ()=> {
-    projects.classList.add("shadow");
+    projects.classList.add("shadows-text");
+    setTimeout(projectsShadow, 3500);
 });
 
+function projectsShadow(){
+    projects.classList.remove("shadows-text");
+}
+
 contactLink.addEventListener('click', ()=> {
-    contact.classList.add("shadow");
+    contact.classList.add("shadows-text");
+    setTimeout(contactShadow, 3500);
 });
+
+function contactShadow(){
+    contact.classList.remove("shadows-text");
+}
